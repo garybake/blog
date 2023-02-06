73aeef483f17f1f53e0c926943871b26c9aa3cab
Gary Bake <garybake@gmail.com>
2023-02-06 08:53:18 +0000

    Added sitemap generator

--- pelicanconf.py
+++ pelicanconf.py
@@ -46,3 +46,17 @@ SOCIAL = (('twitter', 'http://twitter.com/MrGaryBake'),
 SITELOGO = '/images/face.jpg'
 
 PYGMENTS_STYLE = "monokai"
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.5,
+        "indexes": 0.5,
+        "pages": 0.5
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "monthly",
+        "pages": "monthly"
+    }
+}
